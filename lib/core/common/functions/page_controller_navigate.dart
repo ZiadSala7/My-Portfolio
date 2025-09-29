41077311c164cf2e8343a85f81aaea73b0d4e77c
Ziad <zslah2800@gmail.com>
2025-09-29 12:17:59 +0300

    portfolioooooooooooooooo

--- lib/core/common/functions/page_controller_navigate.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-pageControllerNavigate(int index, PageController controller) {
-  controller.animateToPage(
-    index,
-    duration: Duration(milliseconds: 1000),
-    curve: Curves.decelerate,
-  );
-}
--- lib/core/common/functions/url_launcher.dart
+++
@@ -1,8 +0,0 @@
-import 'package:url_launcher/url_launcher.dart';
-
-Future<void> launchURL(String url) async {
-  final Uri link = Uri.parse(url);
-  if (!await launchUrl(link)) {
-    throw Exception('Could not launch $link');
-  }
-}

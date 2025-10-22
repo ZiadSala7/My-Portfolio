85ea185f67136eff18daa26975feda6689ac1ddc
Ziad <zslah2800@gmail.com>
2025-10-22 10:07:04 +0300

    remove device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'my_portfolio.dart';
 
 void main() {
-  runApp(
-    DevicePreview(
-      enabled: !kReleaseMode,
-      builder: (context) => const MyPortfolio(),
-    ),
-  );
+  runApp(MyPortfolio());
 }
